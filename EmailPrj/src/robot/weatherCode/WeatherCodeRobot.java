c4987b730b03ca98f83f8604b70fdda6a5774ad2
kangkangliuz <347579300@qq.com>
2017-10-09 14:02:09 +0800

    分类城市编码

--- EmailPrj/src/robot/weatherCode/WeatherCodeRobot.java
+++ EmailPrj/src/robot/weatherCode/WeatherCodeRobot.java
@@ -1,7 +1,10 @@
 package robot.weatherCode;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,51 +15,63 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WeatherCodeRobot {
-    final static Pattern FINDER = Pattern.compile("^((cn)\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([a-zA-Z]\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([0-9]\\S+) +([0-9]\\S+)$");
+    final static Pattern FINDER = Pattern.compile("((CN)\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([a-zA-Z]\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([a-zA-Z]\\S+) +([\\u4e00-\\u9fa5]\\S+) +([0-9]\\S+) +([0-9]\\S+)");
     public static void main(String[] args) throws IOException {
-        System.out.println("regExp："+FINDER.toString()+"\n------------------------");
-        System.out.println("\n---------------------------------------");
+        System.out.println("regExp："+FINDER.toString()+"\n------------------------");//显示正则表达式
+        //读文件
         FileInputStream fis = new FileInputStream("note\\和风天气中国城市列表 v1.3.txt");
         Reader reader = new InputStreamReader(fis, "Gbk");
         BufferedReader bufferedReader = new BufferedReader(reader);
         for (int j = 0; j < 3; j++) {//去掉标题
-            bufferedReader.readLine();
+            bufferedReader.readLine();//读一行
         }
-        int i = 0;
+        int i = 0;//读几条数据
         String tempStr = null;
-        List<WeatherCodeBean> list = new ArrayList<>();
-        WeatherCodeBean bean  = new WeatherCodeBean();
+        List<WeatherCodeBean> list = new ArrayList<>();//编码bean list
+        WeatherCodeBean bean  = new WeatherCodeBean();//编码bean
         while ((tempStr = bufferedReader.readLine()) != null) {
-            if (i == 1) return;
+            if (i == 4) return;  //第几行结束
             i++;
             System.out.println("==============================\n"+tempStr);
-            tempStr = "CN101010100 beijing 北京  CN  China   中国  beijing 北京  beijing 北京  39.904989   116.405285";
+//            tempStr = "CN101010100 beijing 北京  CN  China   中国  beijing 北京  beijing 北京  39.904989   116.405285";
             Matcher matcher = FINDER.matcher(tempStr);
+            String province,area,city = null,code = null;
+            File file;
             while (matcher.find()) {
-                /*bean.setCity(matcher.group(1));
-                bean.setCityEnName(matcher.group(2));
-                bean.setCityCnName(matcher.group(3));
-                bean.setCountryCode(matcher.group(4));
-                bean.setCountryEnName(matcher.group(5));
-                bean.setCountryCnName(matcher.group(6));
-                bean.setProvinceEnName(matcher.group(7));
-                bean.setProvinceCnName(matcher.group(8));
-                bean.setAreaEnName(matcher.group(9));
-                bean.setAreaCnName(matcher.group(10));
-                bean.setWeidu(matcher.group(11));
-                bean.setJingdu(matcher.group(12));*/
-                System.out.println(matcher.group(1));
-                System.out.println(matcher.group(2));
-                System.out.println(matcher.group(3));
-                System.out.println(matcher.group(4));
-                System.out.println(matcher.group(5));
-                System.out.println(matcher.group(6));
-                System.out.println(matcher.group(7));
-                System.out.println(matcher.group(8));
-                System.out.println(matcher.group(9));
-                System.out.println(matcher.group(10));
-                System.out.println(matcher.group(11));
-                System.out.println(matcher.group(12));
+//                System.out.println(matcher.group(11));//省
+                province = matcher.group(11);
+                file = new File("note\\和风天气中国城市列表v1.3\\"+province);
+                if (!file.exists()) {//不存在该城市文件夹，则创建
+                    System.out.println("创建城市文件夹"+file.mkdirs());
+                }
+//                System.out.println(matcher.group(9));//区
+                area = matcher.group(9);
+                file = new File("note\\和风天气中国城市列表v1.3\\"+province+"\\"+area+".txt");
+                if (!file.exists()) {//不存在该地区文件夹，则创建
+                    System.out.println("创建地区txt"+file.createNewFile());
+                }
+                //打开地区txt文件
+                FileWriter fw = new FileWriter(file,true);
+                city = matcher.group(4);
+                code = matcher.group(1);
+                fw.write(city);
+                fw.write("       ");
+                fw.write(code);
+//                System.out.println(matcher.group(4));//地市
+//                System.out.println(matcher.group(1));//编码
+ //--------------------------------------------------------------------
+//                System.out.println(matcher.group(1));//编码
+//                System.out.println(matcher.group(2));
+//                System.out.println(matcher.group(3));
+//                System.out.println(matcher.group(4));//地市
+//                System.out.println(matcher.group(5));
+//                System.out.println(matcher.group(6));
+//                System.out.println(matcher.group(7));
+//                System.out.println(matcher.group(8));
+//                System.out.println(matcher.group(9));//区
+//                System.out.println(matcher.group(10));
+//                System.out.println(matcher.group(11));//省
+//                System.out.println(matcher.group(12));
             }
 //            System.out.println(bean+"\n==============================");
         }
