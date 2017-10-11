cb3e435adcb1b492d6e5a5f09e9d5c75f7895002
WEIJIE295 <903914294@qq.com>
2017-10-11 11:18:41 +0800

    服务调用框架的基本实现

--- mmp/src/services/impl/WeatherNoticeService.java
+++ mmp/src/services/impl/WeatherNoticeService.java
@@ -1,11 +1,20 @@
 package services.impl;
 
 import services.AbstractService;
-
+/**
+ * 天气预报推送服务
+ * @author cwj
+ *
+ */
 public class WeatherNoticeService extends AbstractService {
 
+	public WeatherNoticeService(){}
+	
+	public WeatherNoticeService(String context){
+		super(context);
+	}
 	@Override
-	public void service() {
+	public void run() {
 		//天气预报通知实现
 	}
 
